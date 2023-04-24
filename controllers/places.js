c48329e5cf3ede4e791278d6a115c6d76805075e
Laura Velasco <maldolt@nv.ccsd.net>
2023-04-23 22:50:31 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -25,8 +26,19 @@ router.get('/', (req, res) => {
 
   })
   router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
 module.exports = router
