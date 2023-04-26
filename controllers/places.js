bf7df3d18fcfb9fa6b1e483f35fdd43d973e3da1
Laura Velasco <maldolt@nv.ccsd.net>
2023-04-26 16:28:16 -0700

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -18,6 +18,18 @@ router.delete('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
 
 
 
