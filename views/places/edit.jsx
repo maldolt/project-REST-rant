17c337a3872946d43a2d3acfdb3c238a3113e88b
Laura Velasco <maldolt@nv.ccsd.net>
2023-04-30 12:50:13 -0700

    added seed place

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,33 +6,51 @@ function edit_form (data) {
         <Def>
       <main>
         <h1>Edit Place</h1>
-        <form method="POST" action={`/places/${data.id}?_method=PUT`}>
-        <div className="form-group">
-            <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" value={data.place.name} required />
-          </div>
-          <div className="form-group">
-            <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" value={data.place.pic}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" value={data.place.city}/>
-          </div>
-          <div className="form-group">
+       <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+  <div className="row">
+      <div className="form-group col-sm-6">
+      <label htmlFor="name">Place Name</label>
+      <input 
+        className="form-control" 
+        id="name" 
+        name="name" 
+        value={data.place.name} 
+      required />
+
+      </div>
+      <div className="form-group col-sm-6">
+          <label htmlFor="pic">Place Picture</label>
+          <input 
+          className="form-control" 
+          id="pic" 
+          name="pic" 
+          value={data.place.pic}/>
+      </div>
+      <div className="form-group col-sm-6">
+          <label htmlFor="city">City</label>
+          <input className="form-control" 
+          id="city" 
+          name="city" 
+          value={data.place.city}/>
+      </div>
+      <div className="form-group col-sm-6">
             <label htmlFor="state">State</label>
-            <input className="form-control" id="state" name="state" value={data.place.state}/>
+            <input 
+            className="form-control" 
+            id="state" 
+            name="state" 
+            value={data.place.state}/>
           </div>
-          <div className="form-group">
+          <div className="form-group col-sm-6">
             <label htmlFor="cuisines">Cuisines</label>
             <input
-              className="form-control"
+              className="form-control col-sm-6"
               id="cuisines"
               name="cuisines"
               value={data.place.cuisines}
-              required
-            />
+              required/>
           </div>
+  </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
       </main>
