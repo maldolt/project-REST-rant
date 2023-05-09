b9d6304472d4bb608d05dc0d20edf6fee9555bee
Laura Velasco <maldolt@nv.ccsd.net>
2023-05-08 20:59:04 -0700

    changed comments content

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,18 +3,36 @@ const Def = require("../default");
 
 function show(data) {
   let comments = <h3 className="inactive">No comments yet!</h3>;
-  let rating = <h3 className="inactive">No ratings yet!</h3>;
-
+  let rating = <h3 className="inactive">Not yet rated</h3>;
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars;
+    }, 0);
+    let averageRating = Math.round(sumRatings / data.place.comments.length);
+    let stars = "";
+    for (let i = 0; i < averageRating; i++) {
+      stars += "⭐️";
+    }
+    rating = <h3>{stars} stars</h3>;
     comments = data.place.comments.map((c) => {
       return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+        <div className="border col-sm-4">
+          <h2 className="rant">{c.rant ? "Rant! 😡" : "Rave! 😻"}</h2>
           <h4>{c.content}</h4>
           <h3>
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+          <form
+            method="POST"
+            action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}
+          >
+            <input
+              type="submit"
+              className="btn btn-danger"
+              value="Delete Comment"
+            />
+          </form>
         </div>
       );
     });
@@ -24,19 +42,31 @@ function show(data) {
       <main>
         <div className="row">
           <div className="col-sm-6">
-            <img src={data.place.pic} alt={data.place.name} width="50%" />
+            <img src={data.place.pic} alt={data.place.name} width={'60%'}/>
             <h3>
               Located in {data.place.city}, {data.place.state}
             </h3>
           </div>
           <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            {rating}
+            <br />
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            <br />
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
               Edit
             </a>
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            {` `}
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
@@ -45,46 +75,39 @@ function show(data) {
         </div>
         <hr />
         <h2>Comments</h2>
-        <div className="row">
-              {comments}
+        <div className="row">{comments}</div>
+        <hr />
+        <h2>Got Your Own Rant or Rave?</h2>
+        <form action={`/places/${data.place.id}/comment`} method="POST">
+          <div className="row">
+            <div className="form-group col-sm-12">
+              <label htmlFor="content">Content</label>
+              <textarea
+                id="content"
+                name="content"
+                className="form-control" required
+              ></textarea>
             </div>
-            <hr />
-            <h2>Got Your Own Rant or Rave?</h2>
-            <form action={`/places/${data.place.id}/comment`} method="POST">
-              <div className="row">
-                <div className="form-group col-sm-12">
-                  <label htmlFor="content">Content</label>
-                  <textarea id="content" name="content" className="form-control"></textarea>
-                </div>
-              </div>
-       
-              <div className="form-group col-md-4">
-                <label htmlFor="validationDefault01" className="form-label">
-                  Author
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="validationDefault01"
-                  value=""
-                  required
-                />
-              </div>
-
-              <div className="form-group col-md-6">
-                <label htmlFor="validationDefault03" className="form-label">
-                  Comment
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="validationDefault03"
-                  value=""
-                  required
-                />
-              </div>
-              <div className="form-group col-md-6">
-                <input
+          </div>
+          <div className="row">
+            <div className="form-group col-sm-4">
+              <label htmlFor="author">Author</label>
+              <input id="author" name="author" className="form-control" required/>
+            </div>
+            <div className="form-group col-sm-4">
+              <label htmlFor="stars">Star Rating</label>
+              <input
+                type="range"
+                step="0.5"
+                min="1"
+                max="5"
+                id="stars"
+                name="stars"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group col-sm-2">
+            <input
                   className="form-check-input"
                   type="checkbox"
                   value=""
@@ -94,25 +117,12 @@ function show(data) {
                   Rant
                 </label>
               </div>
-              <label htmlFor="customRange3" className="form-label">
-                Rating
-              </label>
-              <input
-                type="range"
-                className="form-range"
-                min="0"
-                max="5"
-                step="0.5"
-                id="customRange3"
-              />
-
-              <div className=" form-group col-12">
-                <button className="btn btn-primary" type="submit">
-                  Submit form
-                </button>
-              </div>
-            
-          
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Add Comment"
+          />
         </form>
       </main>
     </Def>
